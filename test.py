c1166ab09fba773ed04635d053ec022a08d919bc
bsgarcia <basilegarcia@gmail.com>
2023-03-10 18:15:27 +0100

    update

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
-import numpy as np
-
-
-class AbstractServer:
-    def __init__(self, name, ip, port):
-        self.name = name
-        self.ip = ip
-        self.port = port
-
-    def __str__(self):
-        return "Server: {}, IP: {}, Port: {}".format(self.name, self.ip, self.port)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __eq__(self, other):
-        return self.name == other.name and self.ip == other.ip and self.port == other.port
-
-    def __hash__(self):
-        return hash(self.name) ^ hash(self.ip) ^ hash(self.port)
-
+import language_tool_python as ltp
+lt = ltp.LanguageToolPublicAPI('en-US')
+# with ltp.LanguageTool('en-US') as lt:
+text = 'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
+matches = lt.check(text)
+print(lt.correct(text))
+print(matches)
 
     
 
